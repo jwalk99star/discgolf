22abc0c461d4b4f95b6d966078ca6d12b7b1a887
Jared Carlson <jaredrcarlson@gmail.com>
2023-07-14 11:13:01 -0600

    first commit

---
+++ discgolf.client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
+export const audience = 'classroom.com'
+export const clientId = 'XFzpsW9hZHurMjX8UM2SA2ZgDOfkh06D'
